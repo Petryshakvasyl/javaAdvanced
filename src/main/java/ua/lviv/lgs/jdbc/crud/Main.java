f565f32c94b0af6461e5004bbed7c204d8bcc41d
Vasyl Petryshak <vasyl.petryshak@globaltechmakers.com>
2020-02-15 19:38:16 +0200

    log4j

--- src/main/java/ua/lviv/lgs/jdbc/crud/Main.java
+++ src/main/java/ua/lviv/lgs/jdbc/crud/Main.java
@@ -1,5 +1,6 @@
 package ua.lviv.lgs.jdbc.crud;
 
+import org.apache.log4j.Logger;
 import ua.lviv.lgs.jdbc.crud.conneciton.ConnectionManager;
 import ua.lviv.lgs.jdbc.crud.entity.Employee;
 import ua.lviv.lgs.jdbc.crud.repository.EmployeeRepository;
@@ -7,16 +8,20 @@ import ua.lviv.lgs.jdbc.crud.repository.imp.EmployeeRepositoryImpl;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
+        Logger log = Logger.getLogger(Main.class);
         Connection connection = ConnectionManager.createConnection();
         EmployeeRepository repository = new EmployeeRepositoryImpl(connection);
 
         Employee employee = new Employee("Jon", "Snow");
-        repository.save(employee);
+        IntStream.range(0, 2).forEach(i -> {
+            log.info("iteration : " + i);
+            repository.save(employee);
+        });
         Employee employeeToUpdate = new Employee(52, "Jonny", "Deept");
         repository.update(employeeToUpdate);
 
--- src/main/java/ua/lviv/lgs/jdbc/crud/repository/imp/EmployeeRepositoryImpl.java
+++ src/main/java/ua/lviv/lgs/jdbc/crud/repository/imp/EmployeeRepositoryImpl.java
@@ -1,5 +1,7 @@
 package ua.lviv.lgs.jdbc.crud.repository.imp;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import ua.lviv.lgs.jdbc.crud.entity.Employee;
 import ua.lviv.lgs.jdbc.crud.mapper.EmployeeMapper;
 import ua.lviv.lgs.jdbc.crud.repository.EmployeeRepository;
@@ -16,6 +18,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     public static final String CREATE = "INSERT INTO employee2 (first_name, last_name) values(?,?)";
     public static final String UPDATE_BY_ID = "UPDATE employee SET first_name=?, last_name=? WHERE id=?";
     private static String READ_BY_ID = "select * from employee where id =?";
+    private final Logger log = Logger.getLogger(EmployeeRepositoryImpl.class);
     private Connection connection;
 
     public EmployeeRepositoryImpl(Connection connection) {
@@ -23,16 +26,22 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     }
 
     public void save(Employee employee) {
+        log.setLevel(Level.ERROR);
+        log.info("save employee ...");
         PreparedStatement statement = null;
         try {
+            log.debug("try to prepare statement ");
             statement = connection.prepareStatement(CREATE);
+            log.debug("statement is ready ... ");
 
             statement.setString(1, employee.getFirstName());
             statement.setString(2, employee.getLastName());
+            log.debug("exequting query ... ");
             statement.execute();
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("after trying to exequte query", e);
+//            e.printStackTrace();
         } finally {
             try {
                 if (statement != null) {
@@ -51,7 +60,6 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
         statement.setString(2, employee.getLastName());
         statement.execute();
     }
-
     private static String DELETE_BY_ID = "delete from employee where id=?";
 
     public void delete(int id) throws SQLException {
@@ -70,7 +78,6 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
         preparedStatement.close();
         return result;
     }
-
     private static String READ_ALL = "select * from employee";
 
     public List<Employee> findAll() throws SQLException {
